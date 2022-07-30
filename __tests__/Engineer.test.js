b3b054556146132553ab337cb6aa942fad3f5774
Salvador Beltran <salbeltran1995@gmail.com>
2022-07-30 16:17:56 -0400

    updates

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,21 +3,21 @@ const Engineer = require('../lib/Engineer');
 
 // Creating engineer object  
 test('creates an Engineer object', () => {
-    const engineer = new Engineer('Nicole', 90, 'nicole.elisaw@gmail', 'nicolewallace09');
+    const engineer = new Engineer('Sal', 90, 'salbeltran1995@gmail.com', 'itsyourpalsal');
     
     expect(engineer.github) .toEqual(expect.any(String));
 });
 
 // Gets github from getGithub()
 test('gets engineer github value', () => {
-    const engineer = new Engineer('Nicole', 90, 'nicole.elisaw@gmail', 'nicolewallace09');
+    const engineer = new Engineer('Sal', 90, 'salbeltran1995@gmail.com', 'itsyourpalsal');
 
     expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
 });
 
 // Gets role from getRole() 
 test('gets role of employee', () => {
-    const engineer = new Engineer('Nicole', 90, 'nicole.elisaw@gmail', 'nicolewallace09');
+    const engineer = new Engineer('Sal', 90, 'salbeltran1995@gmail.com', 'itsyourpalsal');
 
     expect(engineer.getRole()).toEqual("Engineer");
 });
